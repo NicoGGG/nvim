ceca4d8d4681dffc4dcebdb972ffc467dbf618eb
NicoGGG <nicolas.guelfi@achilio.com>
2023-12-25 01:03:56 +0100

    feat: startup screen
    Also added an auto save on InsertLeave coupled with autoformat
    Revert this in case weird thing happen

---
+++ lua/core/autocmd.lua
@@ -0,0 +1,14 @@
+local api = vim.api
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+  command = "silent! lua vim.highlight.on_yank()",
+  group = yankGrp,
+})
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd(
+  "BufReadPost",
+  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
+)
--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -4,11 +4,24 @@ return {
     vim.g.neoformat_enabled_python = { "black" }
     vim.g.neoformat_enabled_htmldjango = { "djlint" }
     vim.g.neoformat_enabled_lua = { "stylua" }
-    vim.cmd([[
-      augroup fmt
-        " autocmd!
-        autocmd BufWritePre * undojoin | Neoformat
-      augroup END
-    ]])
+    local fmt = vim.api.nvim_create_augroup("fmt", { clear = true })
+    vim.api.nvim_create_autocmd("InsertLeave", {
+      group = fmt,
+      command = "undojoin | Neoformat | write",
+    })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = fmt,
+      callback = function()
+        vim.cmd("Neoformat")
+      end,
+    })
+    -- -- Old way of doing it. Was working so I leave it here just in case
+    -- vim.cmd([[
+    --   augroup fmt
+    --     autocmd!
+    --     " autocmd InsertLeave | write
+    --     autocmd BufWritePre * undojoin | Neoformat
+    --   augroup END
+    -- ]])
   end,
 }
