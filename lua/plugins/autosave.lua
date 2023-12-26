c89577ad7b839439204f1f42437a5fa583879283
NicoGGG <coolranger@protonmail.com>
2023-12-26 23:43:50 +0100

    fix: disable autosave for all non-normal buffers

--- lua/plugins/autosave.lua
+++ lua/plugins/autosave.lua
@@ -5,5 +5,23 @@ return {
     -- Next command allows to restore file to its initial state and quit,
     -- effectively doing a "!quit" if autosave was disabled
     vim.keymap.set("n", "<leader>aR", ":u0 | w | q<CR>")
+    -- -- Option to disable auto save on harpoon ui buffers only
+    -- local opts = {
+    --   condition = function(buf)
+    --     if vim.bo[buf].filetype == "harpoon" then
+    --       return false
+    --     end
+    --   end,
+    -- }
+
+    -- Option to disable autosave on all non-normal buffers
+    local opts = {
+      condition = function(buf)
+        if vim.bo[buf].buftype ~= "" then
+          return false
+        end
+      end,
+    }
+    require("auto-save").setup(opts)
   end,
 }
