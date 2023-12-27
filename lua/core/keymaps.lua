7d8da1fd70c2822a4707169fd56cba7b3602490b
NicoGGG <coolranger@protonmail.com>
2023-12-27 14:09:52 +0100

    refacto: some keymaps change

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,31 +3,35 @@ vim.g.mapleader = " "
 
 local map = vim.api.nvim_set_keymap
 local opts = {}
--- Main operations
+-- -- Basic operations
+-- Save with leader w to avoid mistyping ":"
 map("n", "<leader>w", "<cmd>w<CR>", opts)
-
+-- Disable highlights
+map("n", "<leader><CR>", "<cmd>noh<CR>", opts)
+-- Paste without copying the deleted text when in visualmode
+map("v", "<leader>p", '"_dP', opts)
+-- Remap Ctrl-C to Esc because autosave works with Esc but Esc sometimes lags, especially in WSL. Moreover, Ctrl-C is more practical to hit on the keyboard
+map("i", "<C-c>", "<Esc>", opts)
 -- Unbind default bindings for arrow keys
-map('v', '<up>', '<nop>', opts)
-map('v', '<down>', '<nop>', opts)
-map('v', '<left>', '<nop>', opts)
-map('v', '<right>', '<nop>', opts)
+map("v", "<up>", "<nop>", opts)
+map("v", "<down>", "<nop>", opts)
+map("v", "<left>", "<nop>", opts)
+map("v", "<right>", "<nop>", opts)
 
-map("n", "<leader>e", ":Ex<CR>", opts)
+-- Going back to navigation. Replaced by neo-tree show current
+-- map("n", "<leader>e", ":Ex<CR>", opts)
 
 -- Resize window panes, we can use those arrow keys
 -- to help use resize windows - at least we give them some purpose
-map('n', '<up>', '<cmd>resize +2<CR>', opts)
-map('n', '<down>', '<cmd>resize -2<CR>', opts)
-map('n', '<left>', '<cmd>vertical resize -2<CR>', opts)
-map('n', '<right>', '<cmd>vertical resize +2<CR>', opts)
+map("n", "<up>", "<cmd>resize +2<CR>", opts)
+map("n", "<down>", "<cmd>resize -2<CR>", opts)
+map("n", "<left>", "<cmd>vertical resize -2<CR>", opts)
+map("n", "<right>", "<cmd>vertical resize +2<CR>", opts)
 
 -- Text maps
 -- ---------
 -- Move a line of text Alt+[j/k]
-map('n', '<M-j>', [[mz:m+<CR>`z]], opts)
-map('n', '<M-k>', [[mz:m-2<CR>`z]], opts)
-map('v', '<M-j>', [[:m'>+<CR>`<my`>mzgv`yo`z]], opts)
-map('v', '<M-k>', [[:m'<-2<CR>`>my`<mzgv`yo`z]], opts)
-
--- Disable highlights
-map('n', '<leader><CR>', '<cmd>noh<CR>', opts)
+map("n", "<M-j>", [[mz:m+<CR>`z]], opts)
+map("n", "<M-k>", [[mz:m-2<CR>`z]], opts)
+map("v", "<M-j>", [[:m'>+<CR>`<my`>mzgv`yo`z]], opts)
+map("v", "<M-k>", [[:m'<-2<CR>`>my`<mzgv`yo`z]], opts)
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   config = function()
     vim.keymap.set("n", "<leader>ne", ":Neotree toggle left<CR>")
+    vim.keymap.set("n", "<leader>e", ":Neotree focus current<CR>")
     vim.keymap.set("n", "<leader>nf", ":Neotree focus<CR>")
     vim.keymap.set("n", "<leader>nb", ":Neotree toggle buffers right<CR>")
   end,
