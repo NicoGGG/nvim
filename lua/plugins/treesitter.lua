28338b8afd48833b344f4645281e0b71aaeeb29d
NicoGGG <coolranger@protonmail.com>
2024-01-12 13:01:49 +0100

    feat: added bash in ensure installed treesitter to avoid issue with md file with heredoc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -22,6 +22,7 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
+          "bash",
           "c",
           "lua",
           "vim",
