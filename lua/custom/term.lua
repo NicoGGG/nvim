6e1d1bb4b43c4719af1bebf64ecc853c5d0fd21e
Nicolas Guelfi <nicolas.guelfi@winamax.fr>
2024-12-16 12:01:45 +0100

    feat(term): some term commands
    Signed-off-by: Nicolas Guelfi <nicolas.guelfi@winamax.fr>

--- lua/custom/term.lua
+++ lua/custom/term.lua
@@ -7,17 +7,22 @@ vim.keymap.set("n", "<leader>tn", function()
   term_id = vim.bo.channel
 end, { desc = "[T]erminal [N]ew" })
 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-n>", [[<C-\><C-n>]])
 
 -- This is an example shortcut to send to the terminal
 vim.keymap.set("n", "<leader>tl", function()
-  vim.fn.chansend(term_id, { "ls\r\n" })
+  vim.fn.chansend(term_id, { "ls\r" })
 end, { desc = "[T]erminal [L]S" })
 
--- This is an example shortcut to send to the terminal
+-- Rerun last command
 vim.keymap.set("n", "<leader>tr", function()
   vim.fn.chansend(term_id, { "\x1b\x5b\x41\r\n" })
 end, { desc = "[T]erminal [R]erun last command" })
+
+-- Winamax specific: yarn lint
+vim.keymap.set("n", "<leader>twl", function()
+  vim.fn.chansend(term_id, { "yarn lint\r" })
+end, { desc = "[T]erminal [W]inamax yarn [L]int" })
 
 vim.api.nvim_create_autocmd("TermOpen", {
   group = vim.api.nvim_create_augroup("term-open", { clear = true }),
