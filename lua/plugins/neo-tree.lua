5c2a228fa26f82703f000bb2cec20da8863ed725
NicoGGG <coolranger@protonmail.com>
2024-12-19 17:12:20 +0100

    feat(tree)!: trying out oil instead of neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,11 +1,11 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = { "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  config = function()
-    vim.keymap.set("n", "<leader>ne", ":Neotree toggle left<CR>")
-    vim.keymap.set("n", "<leader>e", ":Neotree focus current<CR>", { silent = true })
-    vim.keymap.set("n", "<leader>nf", ":Neotree focus<CR>")
-    vim.keymap.set("n", "<leader>nb", ":Neotree toggle buffers right<CR>")
-  end,
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- branch = "v3.x",
+  -- dependencies = { "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+  -- config = function()
+  --   vim.keymap.set("n", "<leader>ne", ":Neotree toggle left<CR>")
+  --   vim.keymap.set("n", "<leader>e", ":Neotree focus current<CR>", { silent = true })
+  --   vim.keymap.set("n", "<leader>nf", ":Neotree focus<CR>")
+  --   vim.keymap.set("n", "<leader>nb", ":Neotree toggle buffers right<CR>")
+  -- end,
 }
