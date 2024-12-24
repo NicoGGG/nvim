cd676858482a739b53982ca089e4fefdc01d9cee
NicoGGG <coolranger@protonmail.com>
2024-12-24 22:34:16 +0100

    chore: fix diffview and move ftplugins to after

--- ftplugin/markdown.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt_local.conceallevel = 2
--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,11 +3,6 @@ return {
   config = function()
     require("diffview").setup({
       diff_binaries = false,
-      file_panel = {
-        position = "left",
-        width = 35,
-        height = 10,
-      },
     })
   end,
 }
