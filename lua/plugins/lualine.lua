07f8463d28576a6d305aece090eadc690456f217
NicoGGG <coolranger@protonmail.com>
2024-12-24 23:06:23 +0100

    chore(lualine): better status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -55,20 +55,15 @@ return {
           icons_enabled = true,
           component_separators = "|",
           section_separators = "",
+          -- component_separators = { left = "", right = "" }, -- defaults
+          -- section_separators = { left = "", right = "" }, -- defaults
         },
         sections = {
-          lualine_x = {
-            {
-              require("noice").api.statusline.mode.get,
-              cond = require("noice").api.statusline.mode.has,
-              color = { fg = "#ff9e64" },
-            },
-          },
-          lualine_a = {
-            {
-              "buffers",
-            },
-          },
+          lualine_a = { "mode" },
+          lualine_b = { "buffers" },
+          lualine_c = { "branch", "diff", "diagnostics" },
+          lualine_x = { "filename" },
+          lualine_y = { "filetype" },
         },
       })
     end,
